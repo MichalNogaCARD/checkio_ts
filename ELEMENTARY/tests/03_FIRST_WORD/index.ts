4b4a640416b45296f89d1ad029ac83a1151f76b0
Michal <michalnoga.poczta@gmail.com>
2021-04-02 23:12:56 +0200

    ELEMENTARY - 03 First Word

--- ELEMENTARY/tests/03_FIRST_WORD/index.ts
+++ ELEMENTARY/tests/03_FIRST_WORD/index.ts
@@ -5,10 +5,7 @@ import firstWord = FirstWord.firstWord;
 
 describe('FirstWord', () => {
     it('firstWord', () => {
-        console.log('Example:');
-        console.log(firstWord('Hello world'));
 
-// These "asserts" are used for self-checking
         assert.equal(firstWord('Hello world'), 'Hello');
         assert.equal(firstWord('a word'), 'a');
         assert.equal(firstWord('hi'), 'hi');
