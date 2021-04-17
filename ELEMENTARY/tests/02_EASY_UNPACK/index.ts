ef81a9e3ebd6b396d140acf18412f35238f26c90
Michal <michalnoga.poczta@gmail.com>
2021-04-17 23:13:33 +0200

    ELEMENTARY - 02 Easy Unpack

--- ELEMENTARY/tests/02_EASY_UNPACK/index.ts
+++ ELEMENTARY/tests/02_EASY_UNPACK/index.ts
@@ -5,10 +5,7 @@ import easyUnpack = EasyUnpack.easyUnpack;
 
 describe('EasyUnpack', () => {
     it('easyUnpack', () => {
-        console.log('Example:');
-        console.log(easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]));
 
-// These "asserts" are used for self-checking
         assert.deepEqual(easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]), [1, 3, 7]);
         assert.deepEqual(easyUnpack([1, 1, 1, 1]), [1, 1, 1]);
         assert.deepEqual(easyUnpack([6, 3, 7]), [6, 7, 3]);
