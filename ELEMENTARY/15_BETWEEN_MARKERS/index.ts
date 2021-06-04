2ff14f9a72a71ca18ea4b7d2161ab742f959a8c3
Michal <michalnoga.poczta@gmail.com>
2021-06-04 16:28:35 +0200

    ELEMENTARY - 15 Between Markers

--- ELEMENTARY/15_BETWEEN_MARKERS/index.ts
+++ ELEMENTARY/15_BETWEEN_MARKERS/index.ts
@@ -1,5 +1,7 @@
 export module BetweenMarkers {
     export function betweenMarkers(line: string, left: string, right: string): string {
 
+        let withoutLeft = line.substring(line.indexOf(left) + 1);
+        return withoutLeft.substring(0, withoutLeft.indexOf(right));
     }
 }
--- ELEMENTARY/tests/15_BETWEEN_MARKERS/index.ts
+++ ELEMENTARY/tests/15_BETWEEN_MARKERS/index.ts
@@ -1,9 +1,15 @@
 import "mocha";
 import * as assert from 'assert';
+import {BetweenMarkers} from "../../15_BETWEEN_MARKERS";
+import betweenMarkers = BetweenMarkers.betweenMarkers;
 
-describe('BetweenMarkers',()=>{
+describe('BetweenMarkers', () => {
 
-    it('betweenMarkers',()=>{
+    it('betweenMarkers', () => {
 
+        assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple');
+        assert.equal(betweenMarkers('What is [apple]', '[', ']'), 'apple');
+        assert.equal(betweenMarkers('What is ><', '>', '<'), '');
+        assert.equal(betweenMarkers('[an apple]', '[', ']'), 'an apple');
     })
 })
