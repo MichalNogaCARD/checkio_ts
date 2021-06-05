516ce9660637d32160c341dc3bc249a108777fc3
Michal <michalnoga.poczta@gmail.com>
2021-06-05 20:44:50 +0200

    HOME - 13 All The Same

--- HOME/13_ALL_THE_SAME/index.ts
+++ HOME/13_ALL_THE_SAME/index.ts
@@ -1,11 +1,5 @@
 export module AllTheSame {
     export function allTheSame(elements: any[]): boolean {
-
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[0] !== elements[i]) {
-                return false;
-            }
-        }
-        return true;
+        return elements.every(element => element === elements[0]);
     }
 }
